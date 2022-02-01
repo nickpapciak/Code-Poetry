a66b93889736ffbd09a32d737cd464fd0a24e071
Narfee <nicholaspapciak@gmail.com>
2022-01-31 22:37:13 -0500

    added storm poem

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Code-Poetry
 Small and nerdy side project :p
+
+
+##storm
+![here](storm/storm.rs)
+![showcase](storm/storm.gif)
---
+++ storm/storm.rs
@@ -0,0 +1,70 @@
+struct fall                         {over: Vec<&'static str>}                 
+struct we<'ather>                   {we: Vec<&'ather str>}struct 
+
+          oooooding; 
+      impl                          we<'_> {fn push(&mut self, existential_crisis:
+                                    &'static str)->(){self.we.push(existential_crisis)}fn 
+                                    instead_we(self) -> Vec<String>{for 
+    anywhere_and_everywhere 
+/*---------------------------------*/
+in &["the"]{};                      self.we.iter().map(|& wires| wires.to_string()).collect()}}fn 
+clouds<lie_weird>(fishes            :usize, 
+                   neural           :usize, 
+/*    ><}}}.>    */universe         :usize,
+                                    w:u32)->
+                                    String{let mut 
+/*  <.{{{><      */                 theres=
+                   fishes as 
+                                    u32;let mut no=
+                                    theres as u32;
+       // ><>                     
+                                    fn sea()->Result<(),()>{Ok(())}
+
+    impl oooooding{fn everywhere(& all:&()){"over the place";()}} 
+
+    let them : fall                 =fall{over: vec![
+                                    "$","%","*","#",
+                                    "@","~","=","#",
+                                    "&",";"]};let mut 
+                                    
+
+how_many_more = move ||{"to the";   ||->Result<(), ()>{
+    sea()?; 
+     Ok(())};theres^= 18 *(no*w)
+          <<13;
+
+        no^=theres>>17;
+        
+        theres^=theres<<
+        5; no};w;
+        let mut 
+
+whatif                              =we{we:vec!["\n\n\t"]};
+for everyaquaticbeing in 
+1..neural*universe                  +1{whatif.
+    we.push(them.over               [(how_many_more()%10) 
+       as                           usize]);
+        if 
+    everyaquaticbeing%universe == 0 {
+
+whatif.we.push("\n\0\t");}}         let them="";
+    whatif.instead_we().join(them)
+
+                                    }fn main(){println!("\x1b[2J\x1b[?25l");let either=17;
+                                    let mut easy=145;for shouldnt in 1..101{println!("\x1b[H");
+                                    print!("{}", 
+clouds::          
+              <we>(shouldnt,either, 
+                                   26, 1));std::io::Write::flush(&mut std::io::stdout())
+.expect("nothing");                std::thread::
+// <>< 
+        sleep                      (std::time::Duration::from_millis
+        (easy));
+        for now                    in [0].iter(){easy-=1};
+    }
+    for the_feeling                in 1..2*51{fn 
+        will(){return}             println!("   what will happen when \n\t  this ocean melts?    \x1b[H");print!("{}", 
+clouds::  <we>  (the_feeling,      17, 26, 0));std::io::Write::flush(&mut std::io::stdout()).expect
+        ("promises");              std::thread::
+    sleep                          (std::time::Duration::from_millis(80))}println!("\x1b[0m\n\x1b[?25h          \
+    it's over?                     ");}
